58ae90627587d72f9290b93191290ab3231b6e42
xw <xw@qq.com>
2019-12-19 18:36:54 +0800

    支付宝，微信支付

--- src/layuiadmin/js/baseUrlConfig.js
+++ src/layuiadmin/js/baseUrlConfig.js
@@ -1,4 +1,4 @@
 // var BASEURL = "http://192.168.1.188:8092/";
-// var BASEURL = "http://192.168.1.166:9091/";
-var BASEURL = "http://192.168.1.15:8092/";
+var BASEURL = "http://192.168.1.166:8092/";
+// var BASEURL = "http://192.168.1.15:8092/";
 // var BASEURL = "http://132.232.11.63:9091/";
