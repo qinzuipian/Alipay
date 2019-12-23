2b254290ff6a36a6ef2f5d4d2d0d54b94d52db0f
xw <xw@qq.com>
2019-12-23 18:01:50 +0800

    login页面提示修改

--- src/layuiadmin/js/baseUrlConfig.js
+++ src/layuiadmin/js/baseUrlConfig.js
@@ -1,4 +1,5 @@
 // var BASEURL = "http://192.168.1.188:8092/";
 // var BASEURL = "http://192.168.1.166:8092/";
-var BASEURL = "http://192.168.1.15:8092/";
-// var BASEURL = "http://132.232.11.63:9091/";
+// var BASEURL = "http://192.168.1.15:8092/";
+// var BASEURL = "http://132.232.11.63:8092/";
+var BASEURL = "http://106.54.92.42:8092/";
