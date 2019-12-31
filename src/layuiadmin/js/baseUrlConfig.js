a7a055abeb167c3bef6e227b980831e666190460
xw <xw@qq.com>
2019-12-31 14:20:40 +0800

    tijiao

--- src/layuiadmin/js/baseUrlConfig.js
+++ src/layuiadmin/js/baseUrlConfig.js
@@ -1,9 +1,9 @@
 // var BASEURL = "http://192.168.1.188:8092/";
 // var BASEURL = "http://192.168.1.166:8092/";
-// var BASEURL = "http://192.168.1.15:8092/";
+var BASEURL = "http://192.168.1.15:8092/";
 // var BASEURL = "http://132.232.11.63:8092/";
 // var BASEURL = "http://106.54.92.42:8092/";
-var BASEURL = "https://mini.wkang.net/";
+// var BASEURL = "https://mini.wkang.net/";
 // 退款接口
 // var payOrder = "http://106.54.92.42:8081/"
 var payOrder = "https://minipay.wkang.net/"
